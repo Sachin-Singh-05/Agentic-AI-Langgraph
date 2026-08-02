3de2b3fb56f69cebacda0e54e2178293ecb00c9c
Sachin Singh <112792594+Sachin-Singh-05@users.noreply.github.com>
2026-08-02 09:07:57 +0530

    Add files via upload

---
+++ Chatbot/4_chatbot_streaming.py
@@ -0,0 +1,38 @@
+import streamlit as st 
+from Basic_chatbot_backend import chatbot
+from langchain_core.messages import BaseMessage,HumanMessage,AIMessage
+
+CONFIG = {'configurable':{'thread_id': 'thread-1'}}
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history']=[]
+
+# loading the convo history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type Here')
+
+if user_input:
+
+    # first add the message to message history
+    st.session_state['message_history'].append({'role':'user','content':user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    # first add the message to message history
+    with st.chat_message('assistant'):
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config={'configurable': {'thread_id': 'thread-1'}},
+                stream_mode='messages'
+            )
+        )
+
+    st.session_state['message_history'].append(
+        {'role': 'assistant', 'content': ai_message}
+    )
+
+
